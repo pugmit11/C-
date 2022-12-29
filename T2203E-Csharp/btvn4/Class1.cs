6f879ce29dad9dab0ee64483aa63a71fd2882fb3
pugmit11 <tanhtanh1997@gmail.com>
2022-12-29 23:58:41 +0700

    TA

--- T2203E-Csharp/btvn4/Class1.cs
+++ T2203E-Csharp/btvn4/Class1.cs
@@ -4,9 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace T2203E_Csharp.btvn4
+namespace ConsoleApp1.assignment1
 {
-    internal class Class1
+    internal class Fraction
     {
+        private float ts;
+        private float ms;
+        public float Ts
+        {
+            get => ts; set => ts = value;
+        }
+        public float Ms
+        {
+            get => ms;
+            set
+            {
+                if (value != 0)
+                    ms = value;
+            }
+        }
+        public Fraction()
+        {
+
+        }
+        public Fraction(float ts, float ms)
+        {
+            this.ts = ts;
+            this.ms = ms;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("Nhap tu so: ");
+            ts = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Nhap mau so: ");
+                ms = Convert.ToInt32(Console.ReadLine());
+            } while (ms == 0);
+        }
+        public void xuat()
+        {
+            Console.Write($"{Ts}/{Ms}");
+        }
+        public float UCLN(float x, float y)
+        {
+            x = Math.Abs(ts);
+            y = Math.Abs(ms);
+            while (x != y && x != 0 && y != 0)
+            {
+                if (x > y) x = x - y;
+                else y = y - x;
+            }
+            return x;
+        }
+        public Fraction RutGon()
+        {
+            Fraction rutgon = new Fraction();
+            float uoc = UCLN(ts, ms);
+            if (uoc != 0)
+            {
+                rutgon.ts = ts / uoc;
+                rutgon.ms = ms / uoc;
+            }
+            else
+            {
+                rutgon.ms = ms;
+                rutgon.ts = ts;
+            }
+            return rutgon;
+        }
     }
 }
